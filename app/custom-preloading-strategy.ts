12e7e71835b890d7663569b782c2f2d3ecdbc271
Panayot Cankov <Panayot.Cankov@telerik.com>
2016-11-30 13:17:28 +0200

    Use external plugin for requestIdleFrame

--- app/custom-preloading-strategy.ts
+++ app/custom-preloading-strategy.ts
@@ -1,59 +1,11 @@
+import * as app from "application";
+
 import { PreloadingStrategy, Route } from "@angular/router";
 import { _throw } from "rxjs/observable/throw";
 import { _catch } from "rxjs/operator/catch";
 import { Observable } from "rxjs";
 
-import * as utils from "utils/utils";
-import {isAndroid, isIOS} from "platform";
-
-declare var interop, NSPostWhenIdle;
-
-let requestIdleFrame: (callback: () => void) => void;
-if (isAndroid) {
-    console.log("TODO: requestIdleFrame for Android");
-    requestIdleFrame = cb => cb();
-} else if (isIOS) {
-    const queue = [];
-
-    class TNSIdleHandler extends NSObject {
-        public static ObjCExposedMethods = {
-            "receiveIdleNotification": { returns: interop.types.void, params: [] }
-        };
-        receiveIdleNotification() {
-            idleNotificationPosted = false;
-            // console.log("Idle! handle something from the idle queue!");
-            try {
-                queue.shift()();
-            } catch(e) {
-                console.log("Error executing on idle frame: " + e);
-                console.log(e.stack);
-            }
-            if (queue.length > 0) {
-                postIdleNotification();
-            } else {
-                console.log("idle frames drained");
-            }
-        }
-    }
-
-    const target = TNSIdleHandler.alloc().init();
-    utils.ios.getter(NSNotificationCenter, NSNotificationCenter.defaultCenter).addObserverSelectorNameObject(target, "receiveIdleNotification", "org.nativescript.idle-notification", null);
-    const idleNotification = NSNotification.notificationWithNameObject("org.nativescript.idle-notification", target);
-    let idleNotificationPosted: boolean = false;
-
-    let postIdleNotification = () => {
-        utils.ios.getter(NSNotificationQueue, NSNotificationQueue.defaultQueue).enqueueNotificationPostingStyle(idleNotification, NSPostWhenIdle);
-        idleNotificationPosted = true;
-    }
-
-    requestIdleFrame = callback => {
-        queue.push(callback);
-        if (!idleNotificationPosted) {
-            console.log("enqueued idle frames");
-            postIdleNotification();
-        }
-    }
-}
+import { requestIdleFrame } from "nativescript-idle";
 
 export class Preload implements PreloadingStrategy {
     private loadedPaths: string[];
@@ -73,6 +25,7 @@ export class Preload implements PreloadingStrategy {
 
         requestIdleFrame(() => {
             var msg = "preloading " + route.path;
+            console.log(msg);
             var start = Date.now();
             load();
             this.loadedPaths.push(route.path);
